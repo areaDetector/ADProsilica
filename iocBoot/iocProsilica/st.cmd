4bc552d7a51de76ca0665c8a350b2ca6c9029d3a
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-03-16 19:24:10 +0000

    Moved from ccd/iocBoot/iocProsilicaCCD
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@6798 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

---
+++ iocBoot/iocProsilica/st.cmd
@@ -0,0 +1,26 @@
+< envPaths
+errlogInit(20000)
+
+dbLoadDatabase("$(AD)/dbd/prosilicaApp.dbd")
+prosilicaCCDApp_registerRecordDeviceDriver(pdbbase) 
+dbLoadRecords("$(AD)/ADApp/Db/ADAsyn.db","P=13PS:,D=cam1:,PORT=PS1,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(CCD)/ADApp/Db/ADAsynImage.db","P=13PS:,D=cam1:,PORT=PS1,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NPIXELS=1392640")
+
+prosilicaSetup(1)
+prosilicaConfig(0, "164.54.160.200")
+drvAsynAreaDetectorConfigure("PS1", "ADProsilica", 0)
+
+#asynSetTraceMask("PS1",0,255)
+
+set_requestfile_path("./")
+set_savefile_path("./autosave")
+set_requestfile_path("$(AD)/AApp/Db")
+set_pass0_restoreFile("auto_settings.sav")
+set_pass1_restoreFile("auto_settings.sav")
+save_restoreSet_status_prefix("13PS:")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13PS:")
+
+iocInit()
+
+# save things every thirty seconds
+create_monitor_set("auto_settings.req", 30)
