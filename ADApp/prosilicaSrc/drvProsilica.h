3894fe6344b2e5a5ddbb65b83aaa9eaba26575b8
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-03-30 17:16:04 +0000

    Added driver table
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@6915 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/prosilicaSrc/drvProsilica.h
+++ ADApp/prosilicaSrc/drvProsilica.h
@@ -1,3 +1,13 @@
+/* drvProsilica.h
+ *
+ * This is a driver for Prosilica cameras (GigE and CameraLink).
+ *
+ * Author: Mark Rivers
+ *         University of Chicago
+ *
+ * Created:  March 20, 2008
+ *
+ */
 #ifndef DRV_PROSILICA_H
 #define DRV_PROSILICA_H
 
@@ -5,8 +15,8 @@
 extern "C" {
 #endif
 
+extern ADDrvSet_t ADProsilica; 
 int prosilicaSetup(int num_cameras);      /* Number of Prosilica cameras in system.  */
-
 int prosilicaConfig(int camera,           /* Camera number */
                     char *ipAddr);        /* IP address of this camera.  If NULL then use first camera found */
 
