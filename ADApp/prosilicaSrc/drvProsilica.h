c2966bc6fbaff0ed1b9288ab4c4f95439ce5e0f7
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-04-03 03:25:26 +0000

    Moved parameter definitions to this file
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@6965 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/prosilicaSrc/drvProsilica.h
+++ ADApp/prosilicaSrc/drvProsilica.h
@@ -15,10 +15,61 @@
 extern "C" {
 #endif
 
-extern ADDrvSet_t ADProsilica; 
-int prosilicaSetup(int num_cameras);      /* Number of Prosilica cameras in system.  */
-int prosilicaConfig(int camera,           /* Camera number */
-                    char *ipAddr);        /* IP address of this camera.  If NULL then use first camera found */
+/* If we have any private driver commands they begin with ADFirstDriverCommand and should end
+   with ADLastDriverCommand, which is used for setting the size of the parameter library table */
+typedef enum {
+    /* These parameters describe the trigger modes of the Prosilica
+     * They must agree with the values in the mbbo/mbbi records in
+     * the Prosilca database. */
+    PSTriggerStartFreeRun,
+    PSTriggerStartSyncIn1,
+    PSTriggerStartSyncIn2,
+    PSTriggerStartSyncIn3,
+    PSTriggerStartSyncIn4,
+    PSTriggerStartFixedRate,
+    PSTriggerStartSoftware
+} PSTriggerStartMode_t;
+
+static char *PSTriggerStartStrings[] = {
+    "Freerun","SyncIn1","SyncIn2","SyncIn3","SyncIn4","FixedRate","Software"
+};
+ 
+#define NUM_START_TRIGGER_MODES (sizeof(PSTriggerStartStrings) / sizeof(PSTriggerStartStrings[0]))
+
+typedef enum {
+    /* These parameters are for the camera statistics */
+    PSReadStatistics = ADFirstDriverParam,
+    PSStatDriverType,
+    PSStatFilterVersion,
+    PSStatFrameRate,
+    PSStatFramesCompleted,
+    PSStatFramesDropped,
+    PSStatPacketsErroneous,
+    PSStatPacketsMissed,
+    PSStatPacketsReceived,
+    PSStatPacketsRequested,
+    PSStatPacketsResent,
+    ADLastDriverParam
+} PSDetParam_t;
+
+static ADParamString_t PSDetParamString[] = {
+    {PSReadStatistics,        "PS_READ_STATISTICS"},
+    {PSStatDriverType,        "PS_DRIVER_TYPE"},
+    {PSStatFilterVersion,     "PS_FILTER_VERSION"},
+    {PSStatFrameRate,         "PS_FRAME_RATE"},
+    {PSStatFramesCompleted,   "PS_FRAMES_COMPLETED"},
+    {PSStatFramesDropped,     "PS_FRAMES_DROPPED"},
+    {PSStatPacketsErroneous,  "PS_PACKETS_ERRONEOUS"},
+    {PSStatPacketsMissed,     "PS_PACKETS_MISSED"},
+    {PSStatPacketsReceived,   "PS_PACKETS_RECEIVED"},
+    {PSStatPacketsRequested,  "PS_PACKETS_REQUESTED"},
+    {PSStatPacketsResent,     "PS_PACKETS_RESENT"}
+};
+
+#define NUM_PS_DET_PARAMS (sizeof(PSDetParamString)/sizeof(PSDetParamString[0]))
+
+int prosilicaConfig(char *portName,       /* Camera number */
+                    int uniqueId);        /* Unique ID number of the camera to use */
 
 #ifdef __cplusplus
 }
