c72323cef3f51db68e52fea25b219d7f0a5f5f8f
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-05-03 13:38:08 +0000

    Added NDPluginBase.template
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@7201 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocProsilica/st.cmd
+++ iocBoot/iocProsilica/st.cmd
@@ -9,28 +9,31 @@ NDArrayBuffInit(50, 200000000)
 
 #prosilicaConfig("PS1", 50110)
 prosilicaConfig("PS1", 51031)
-dbLoadRecords("$(AD)/ADApp/Db/ADBase.template","P=13PS1:,D=cam1:,PORT=PS1,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AD)/ADApp/Db/prosilica.template","P=13PS1:,D=cam1:,PORT=PS1,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",   "P=13PS1:,R=cam1:,PORT=PS1,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/prosilica.template","P=13PS1:,R=cam1:,PORT=PS1,ADDR=0,TIMEOUT=1")
 
 prosilicaConfig("PS2", 50022)
-dbLoadRecords("$(AD)/ADApp/Db/ADBase.template","P=13PS1:,D=cam2:,PORT=PS2,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AD)/ADApp/Db/prosilica.template","P=13PS1:,D=cam2:,PORT=PS2,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",   "P=13PS1:,R=cam2:,PORT=PS2,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/prosilica.template","P=13PS1:,R=cam2:,PORT=PS2,ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin, set it to get data from first simDetector driver.
 drvNDStdArraysConfigure("PS1Image", 5, 0, "PS1", 0)
-dbLoadRecords("$(AD)/ADApp/Db/NDStdArrays.template","P=13PS1:,A=image1:,PORT=PS1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
+dbLoadRecords("$(AD)/ADApp/Db/NDPluginBase.template","P=13PS1:,R=image1:,PORT=PS1Image,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS1,NDARRAY_ADDR=0")
+dbLoadRecords("$(AD)/ADApp/Db/NDStdArrays.template", "P=13PS1:,R=image1:,PORT=PS1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
 
 # Create a file saving plugin
 drvNDFileConfigure("PS1File", 5, 0, "PS1", 0)
-dbLoadRecords("$(AD)/ADApp/Db/NDFile.template","P=13PS1:,F=file1:,PORT=PS1File,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/NDPluginBase.template","P=13PS1:,R=file1:,PORT=PS1File,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS1,NDARRAY_ADDR=0")
+dbLoadRecords("$(AD)/ADApp/Db/NDFile.template",      "P=13PS1:,R=file1:,PORT=PS1File,ADDR=0,TIMEOUT=1")
 
 # Create an ROI plugin
 drvNDROIConfigure("PS1ROI", 5, 0, "PS1", 0, 4)
-dbLoadRecords("$(AD)/ADApp/Db/NDROI.template","P=13PS1:,R=ROI1:,PORT=PS1ROI,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AD)/ADApp/Db/NDROIN.template","P=13PS1:,R=ROI1:0:,PORT=PS1ROI,ADDR=0,TIMEOUT=1,HIST_SIZE=256")
-dbLoadRecords("$(AD)/ADApp/Db/NDROIN.template","P=13PS1:,R=ROI1:1:,PORT=PS1ROI,ADDR=1,TIMEOUT=1,HIST_SIZE=256")
-dbLoadRecords("$(AD)/ADApp/Db/NDROIN.template","P=13PS1:,R=ROI1:2:,PORT=PS1ROI,ADDR=2,TIMEOUT=1,HIST_SIZE=256")
-dbLoadRecords("$(AD)/ADApp/Db/NDROIN.template","P=13PS1:,R=ROI1:3:,PORT=PS1ROI,ADDR=3,TIMEOUT=1,HIST_SIZE=256")
+dbLoadRecords("$(AD)/ADApp/Db/NDPluginBase.template","P=13PS1:,R=ROI1:,  PORT=PS1ROI,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS1,NDARRAY_ADDR=0")
+dbLoadRecords("$(AD)/ADApp/Db/NDROI.template",       "P=13PS1:,R=ROI1:,  PORT=PS1ROI,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/NDROIN.template",      "P=13PS1:,R=ROI1:0:,PORT=PS1ROI,ADDR=0,TIMEOUT=1,HIST_SIZE=256")
+dbLoadRecords("$(AD)/ADApp/Db/NDROIN.template",      "P=13PS1:,R=ROI1:1:,PORT=PS1ROI,ADDR=1,TIMEOUT=1,HIST_SIZE=256")
+dbLoadRecords("$(AD)/ADApp/Db/NDROIN.template",      "P=13PS1:,R=ROI1:2:,PORT=PS1ROI,ADDR=2,TIMEOUT=1,HIST_SIZE=256")
+dbLoadRecords("$(AD)/ADApp/Db/NDROIN.template",      "P=13PS1:,R=ROI1:3:,PORT=PS1ROI,ADDR=3,TIMEOUT=1,HIST_SIZE=256")
 
 
 #asynSetTraceMask("PS1",0,255)
