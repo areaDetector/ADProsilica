198c33a4af0614d9cc722b7fb2c859657c556950
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-05-16 17:24:47 +0000

    Added new parameters for NDArrayPool; increased queue size for NDROI
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@7332 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocProsilica/st.cmd
+++ iocBoot/iocProsilica/st.cmd
@@ -4,20 +4,17 @@ errlogInit(20000)
 dbLoadDatabase("$(AD)/dbd/prosilicaApp.dbd")
 prosilicaApp_registerRecordDeviceDriver(pdbbase) 
 
-# Initialize the buffer library
-NDArrayBuffInit(50, 200000000)
-
-#prosilicaConfig("PS1", 50110)
-prosilicaConfig("PS1", 51031)
+prosilicaConfig("PS1", 50110, 50, 200000000)
+#prosilicaConfig("PS1", 50022)
 dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",   "P=13PS1:,R=cam1:,PORT=PS1,ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AD)/ADApp/Db/prosilica.template","P=13PS1:,R=cam1:,PORT=PS1,ADDR=0,TIMEOUT=1")
 
-prosilicaConfig("PS2", 50022)
+prosilicaConfig("PS2", 50022, 10, 50000000)
 dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",   "P=13PS1:,R=cam2:,PORT=PS2,ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AD)/ADApp/Db/prosilica.template","P=13PS1:,R=cam2:,PORT=PS2,ADDR=0,TIMEOUT=1")
 
-# Create a standard arrays plugin, set it to get data from first simDetector driver.
-drvNDStdArraysConfigure("PS1Image", 5, 0, "PS1", 0)
+# Create a standard arrays plugin, set it to get data from first Prosilica driver.
+drvNDStdArraysConfigure("PS1Image", 5, 0, "PS1", 0, -1)
 dbLoadRecords("$(AD)/ADApp/Db/NDPluginBase.template","P=13PS1:,R=image1:,PORT=PS1Image,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS1,NDARRAY_ADDR=0")
 dbLoadRecords("$(AD)/ADApp/Db/NDStdArrays.template", "P=13PS1:,R=image1:,PORT=PS1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
 
@@ -27,7 +24,7 @@ dbLoadRecords("$(AD)/ADApp/Db/NDPluginBase.template","P=13PS1:,R=file1:,PORT=PS1
 dbLoadRecords("$(AD)/ADApp/Db/NDFile.template",      "P=13PS1:,R=file1:,PORT=PS1File,ADDR=0,TIMEOUT=1")
 
 # Create an ROI plugin
-drvNDROIConfigure("PS1ROI", 5, 0, "PS1", 0, 4)
+drvNDROIConfigure("PS1ROI", 5, 0, "PS1", 0, 10, -1)
 dbLoadRecords("$(AD)/ADApp/Db/NDPluginBase.template","P=13PS1:,R=ROI1:,  PORT=PS1ROI,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS1,NDARRAY_ADDR=0")
 dbLoadRecords("$(AD)/ADApp/Db/NDROI.template",       "P=13PS1:,R=ROI1:,  PORT=PS1ROI,ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AD)/ADApp/Db/NDROIN.template",      "P=13PS1:,R=ROI1:0:,PORT=PS1ROI,ADDR=0,TIMEOUT=1,HIST_SIZE=256")
