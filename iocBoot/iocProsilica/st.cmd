5dca48aa24c047f0ef712a83c6028e874c35a1aa
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-12-24 21:43:16 +0000

    Added color convert plugin; increase memory size of NDStdArrays to support color
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@8112 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocProsilica/st.cmd
+++ iocBoot/iocProsilica/st.cmd
@@ -1,4 +1,3 @@
-< envPaths
 errlogInit(20000)
 
 dbLoadDatabase("$(AREA_DETECTOR)/dbd/prosilicaApp.dbd")
@@ -6,7 +5,7 @@ dbLoadDatabase("$(AREA_DETECTOR)/dbd/prosilicaApp.dbd")
 prosilicaApp_registerRecordDeviceDriver(pdbbase) 
 
 #prosilicaConfig("PS1", 50110, 50, 200000000)
-prosilicaConfig("PS1", 50022, 50, 200000000)
+prosilicaConfig("PS1", 51031, 50, 200000000)
 asynSetTraceIOMask("PS1",0,2)
 #asynSetTraceMask("PS1",0,255)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=13PS1:,R=cam1:,PORT=PS1,ADDR=0,TIMEOUT=1")
@@ -20,7 +19,9 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=13PS1:,R=cam1:,P
 # Create a standard arrays plugin, set it to get data from first Prosilica driver.
 drvNDStdArraysConfigure("PS1Image", 5, 0, "PS1", 0, -1)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13PS1:,R=image1:,PORT=PS1Image,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS1,NDARRAY_ADDR=0")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=13PS1:,R=image1:,PORT=PS1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=13PS1:,R=image1:,PORT=PS1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=4177920")
+# Load the database to use with Stephen Mudie's IDL code
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/EPICS_AD_Viewer.template", "P=13PS1:, R=image1:")
 
 # Create a file saving plugin
 drvNDFileConfigure("PS1File", 5, 0, "PS1", 0)
@@ -36,6 +37,14 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=13PS1:,R=ROI1
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=13PS1:,R=ROI1:2:,PORT=PS1ROI,ADDR=2,TIMEOUT=1,HIST_SIZE=256")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=13PS1:,R=ROI1:3:,PORT=PS1ROI,ADDR=3,TIMEOUT=1,HIST_SIZE=256")
 
+# Create 2 color conversion plugins
+drvNDColorConvertConfigure("PS1CC1", 5, 0, "PS1", 0, -1)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","   P=13PS1:,R=CC1:,  PORT=PS1CC1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS1,NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDColorConvert.template", "P=13PS1:,R=CC1:,  PORT=PS1CC1,ADDR=0,TIMEOUT=1")
+drvNDColorConvertConfigure("PS1CC2", 5, 0, "PS1", 0, -1)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","   P=13PS1:,R=CC2:,  PORT=PS1CC2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS1CC1,NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDColorConvert.template", "P=13PS1:,R=CC2:,  PORT=PS1CC2,ADDR=0,TIMEOUT=1")
+
 
 #asynSetTraceMask("PS1",0,255)
 
@@ -48,6 +57,8 @@ save_restoreSet_status_prefix("13PS1:")
 dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13PS1:")
 
 iocInit()
+
+#asynSetTraceMask("PS1",0,1)
 
 # save things every thirty seconds
 create_monitor_set("auto_settings.req", 30,"P=13PS1:,D=cam1:")
