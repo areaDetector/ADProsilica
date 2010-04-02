78d6874857eaf0f0e1e1bdfeca2ffd558e61f695
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-04-02 14:21:38 +0000

    Simplified significantly by using commonPluginSettings; use PREFIX, PORT and QSIZE macros
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@10514 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocProsilica/st.cmd
+++ iocBoot/iocProsilica/st.cmd
@@ -14,8 +14,10 @@ epicsEnvSet("QSIZE",  "20")
 # The Unique ID will be displayed on the first line in the information window.
 #prosilicaConfig("$(PORT)", 50110, 50, 200000000)
 prosilicaConfig("$(PORT)", 51031, 50, -1)
+
 asynSetTraceIOMask("$(PORT)",0,2)
 #asynSetTraceMask("$(PORT)",0,255)
+
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 # Note that prosilica.template must be loaded after NDFile.template to replace the file format correctly
@@ -28,15 +30,15 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=cam1
 # Create a standard arrays plugin, set it to get data from first Prosilica driver.
 NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, -1)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
+
 # Use this line if you only want to use the Prosilica in 8-bit mode.  It uses an 8-bit waveform record
 # NELEMENTS is set large enough for a 1360x1024x3 image size, which is the number of pixels in RGB images from the GC1380CH color camera. 
 # Must be at least as big as the maximum size of your camera images
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=4177920")
+
 # Use this line if you want to use the Prosilica in 8,12 or 16-bit modes.  
 # It uses an 16-bit waveform record, so it uses twice the memory and bandwidth required for only 8-bit data.
 #dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=4177920")
-# Load the database to use with Stephen Mudie's IDL code
-#dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/EPICS_AD_Viewer.template", "P=$(PREFIX), R=image1:")
 
 # Load all other plugins using commonPlugins.cmd
 < ../commonPlugins.cmd
