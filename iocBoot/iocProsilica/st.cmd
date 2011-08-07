3e0c4e882b9062fc2689b4d09bc623c83045edf6
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2011-08-07 16:26:26 +0000

    Added comments on prosilicaConfig
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@13359 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocProsilica/st.cmd
+++ iocBoot/iocProsilica/st.cmd
@@ -11,13 +11,18 @@ epicsEnvSet("XSIZE",  "1360")
 epicsEnvSet("YSIZE",  "1024")
 epicsEnvSet("NCHANS", "2048")
 
-# The second parameter to the prosilicaConfig command is the uniqueId of the camera.
+# prosilicaConfig(portName,    # The name of the asyn port to be created
+#                 cameraId,    # Unique ID, IP address, or IP name of the camera
+#                 maxBuffers,  # Maximum number of NDArray buffers driver can allocate. -1=unlimited
+#                 maxMemory,   # Maximum memory bytes driver can allocate. -1=unlimited
+#                 priority,    # EPICS thread priority for asyn port driver 0=default
+#                 stackSize,   # EPICS thread stack size for asyn port driver 0=default
+#                 maxPvAPIFrames) # Number of frames to allocate in PvAPI driver. Default=2.
 # The simplest way to determine the uniqueId of a camera is to run the Prosilica GigEViewer application, 
 # select the camera, and press the "i" icon on the bottom of the main window to show the camera information for this camera. 
 # The Unique ID will be displayed on the first line in the information window.
-#prosilicaConfig("$(PORT)", 50110, 50, 200000000)
+#prosilicaConfig("$(PORT)", 51031, 50, -1, 0, 0, 10)
 prosilicaConfig("$(PORT)", 51031, 50, -1)
-#prosilicaConfig("$(PORT)", 101271, 50, -1)
 
 asynSetTraceIOMask("$(PORT)",0,2)
 #asynSetTraceMask("$(PORT)",0,255)
